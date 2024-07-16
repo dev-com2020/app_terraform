07404cb285d063cb55a8c43753e8409beda2d993
DEV-COM <kwp@tlen.pl>
2024-07-16 11:44:02 +0200

    last 5

--- main.tf
+++ main.tf
@@ -13,15 +13,63 @@ provider "aws" {
   region = var.aws_region
 }
 
-# resource "aws_vpc" "tomeczkowa" {
-#     cidr_block = "10.0.0.0/16"
-# }
+resource "aws_security_group" "web_server_sg" {
+    name = "web_server_sg"
+    description = "Allow http and shh traffic"
+    vpc_id = "vpc-0023a48bafb84f43c"
 
-resource "aws_instance" "example" {
-  for_each = toset(var.instance_type)
-  ami = var.ami_id
-  instance_type = each.value
-  tags = {
-    Name = "Instancja-${each.key}"
-  }
+    ingress = {
+        from_port = [22,80,443]
+        to_port = [22,80,443]
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # ingress = {
+    #     from_port = 443
+    #     to_port = 443
+    #     protocol = "tcp"
+    #     cidr_blocks = ["0.0.0.0/0"]
+    # }
+
+    # ingress = {
+    #     from_port = 80
+    #     to_port = 80
+    #     protocol = "tcp"
+    #     cidr_blocks = ["0.0.0.0/0"]
+    # }
+
+    egress = {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+      Name = "web_server_sg${instance_count}"
+    }
 }
+
+resource "aws_instance" "web_server" {
+    count = 2
+    ami = var.ami_id
+    instance_type = "t2-micro"
+    security_groups = [aws_security_group.web_server_sg.name]
+
+    provisioner "remote-exec" {
+        inline = [ 
+            "sudo apt-get update",
+            "sudo apt-get install -y nginx",
+         ]
+      
+    }
+
+    tags = {
+      Name = "web_server_${count.index + 1}"
+    }
+  
+}
+
+
+
